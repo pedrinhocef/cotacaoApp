5cbd231746ed0c5ad3305c30100e7c7f32b91727
Pedro Soares <pedrosoares@brq.com>
2019-02-08 20:08:29 -0200

    iniciando camada de serviço

--- app/src/main/java/com/pedrosoares/cotacaoapp/data/entity/USDT.java
+++ app/src/main/java/com/pedrosoares/cotacaoapp/data/entity/USDT.java
@@ -2,30 +2,126 @@ package com.pedrosoares.cotacaoapp.data.entity;
 
 import com.google.gson.annotations.SerializedName;
 
-public class USDT {
+public class USDT{
 
-    @SerializedName("code")
-    public String code;
-    @SerializedName("codein")
-    public String codein;
-    @SerializedName("name")
-    public String name;
-    @SerializedName("high")
-    public String high;
-    @SerializedName("low")
-    public String low;
-    @SerializedName("pctChange")
-    public String pctChange;
-    @SerializedName("open")
-    public String open;
-    @SerializedName("bid")
-    public String bid;
-    @SerializedName("ask")
-    public String ask;
-    @SerializedName("varBid")
-    public String varBid;
-    @SerializedName("timestamp")
-    public String timestamp;
-    @SerializedName("create_date")
-    public String createDate;
+	@SerializedName("varBid")
+	private String varBid;
+
+	@SerializedName("high")
+	private String high;
+
+	@SerializedName("pctChange")
+	private String pctChange;
+
+	@SerializedName("code")
+	private String code;
+
+	@SerializedName("low")
+	private String low;
+
+	@SerializedName("ask")
+	private String ask;
+
+	@SerializedName("name")
+	private String name;
+
+	@SerializedName("codein")
+	private String codein;
+
+	@SerializedName("bid")
+	private String bid;
+
+	@SerializedName("create_date")
+	private String createDate;
+
+	@SerializedName("timestamp")
+	private String timestamp;
+
+	public void setVarBid(String varBid){
+		this.varBid = varBid;
+	}
+
+	public String getVarBid(){
+		return varBid;
+	}
+
+	public void setHigh(String high){
+		this.high = high;
+	}
+
+	public String getHigh(){
+		return high;
+	}
+
+	public void setPctChange(String pctChange){
+		this.pctChange = pctChange;
+	}
+
+	public String getPctChange(){
+		return pctChange;
+	}
+
+	public void setCode(String code){
+		this.code = code;
+	}
+
+	public String getCode(){
+		return code;
+	}
+
+	public void setLow(String low){
+		this.low = low;
+	}
+
+	public String getLow(){
+		return low;
+	}
+
+	public void setAsk(String ask){
+		this.ask = ask;
+	}
+
+	public String getAsk(){
+		return ask;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setCodein(String codein){
+		this.codein = codein;
+	}
+
+	public String getCodein(){
+		return codein;
+	}
+
+	public void setBid(String bid){
+		this.bid = bid;
+	}
+
+	public String getBid(){
+		return bid;
+	}
+
+	public void setCreateDate(String createDate){
+		this.createDate = createDate;
+	}
+
+	public String getCreateDate(){
+		return createDate;
+	}
+
+	public void setTimestamp(String timestamp){
+		this.timestamp = timestamp;
+	}
+
+	public String getTimestamp(){
+		return timestamp;
+	}
 }
--- app/src/main/java/com/pedrosoares/cotacaoapp/data/remote/service/Api.java
+++ app/src/main/java/com/pedrosoares/cotacaoapp/data/remote/service/Api.java
@@ -1,14 +1,13 @@
 package com.pedrosoares.cotacaoapp.data.remote.service;
 
-import com.pedrosoares.cotacaoapp.data.entity.Coins;
+import com.pedrosoares.cotacaoapp.data.entity.CoinsResponse;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 
 public interface Api {
 
     @GET("all")
-    Observable<Coins> getCoins(@Path("all")String all);
+    Observable<CoinsResponse> getCoins();
 
 }

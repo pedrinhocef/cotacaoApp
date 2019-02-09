ba94af547f8a8e4a6053c2340814fac0b35a4b8c
Carlos Henrique <chsl200293@gmail.com>
2019-02-09 01:16:11 -0200

    corrigindo erro do butterknife

--- app/src/main/java/com/pedrosoares/cotacaoapp/data/remote/service/Api.java
+++ app/src/main/java/com/pedrosoares/cotacaoapp/data/remote/service/Api.java
@@ -1,13 +1,13 @@
 package com.pedrosoares.cotacaoapp.data.remote.service;
 
-import com.pedrosoares.cotacaoapp.data.entity.CoinsResponse;
-
-import io.reactivex.Observable;
-import retrofit2.http.GET;
+//import com.pedrosoares.cotacaoapp.data.entity.CoinsResponse;
+//
+//import io.reactivex.Observable;
+//import retrofit2.http.GET;
 
 public interface Api {
 
-    @GET("all")
-    Observable<CoinsResponse> getCoins();
+//    @GET("all")
+//    Observable<CoinsResponse> getCoins();
 
 }

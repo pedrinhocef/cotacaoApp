7439e8b0e92021bdc13ea1a0579d51c8783799b3
Carlos Henrique <chsl200293@gmail.com>
2019-02-10 01:45:51 -0200

    criacao da camada de apresentação

---
+++ app/src/main/java/com/pedrosoares/cotacaoapp/presentation/activity/CotacaoActivity.java
@@ -0,0 +1,15 @@
+package com.pedrosoares.cotacaoapp.presentation.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pedrosoares.cotacaoapp.R;
+
+public class CotacaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cotacao);
+    }
+}

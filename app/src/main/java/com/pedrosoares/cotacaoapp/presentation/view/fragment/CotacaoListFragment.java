f9dcaba4e9c5451b673615ae7b2c5d45db2016e0
Pedro Soares <pedrinhocef@gmail.com>
2019-03-08 16:07:59 -0300

    add snackBar

--- app/src/main/java/com/pedrosoares/cotacaoapp/presentation/view/fragment/CotacaoListFragment.java
+++ app/src/main/java/com/pedrosoares/cotacaoapp/presentation/view/fragment/CotacaoListFragment.java
@@ -3,12 +3,12 @@ package com.pedrosoares.cotacaoapp.presentation.view.fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.pedrosoares.cotacaoapp.R;
 import com.pedrosoares.cotacaoapp.core.base.BaseFragment;
@@ -69,10 +69,12 @@ public class CotacaoListFragment extends BaseFragment<CoinsContract.CoinsListPre
         super.onResume();
         if (isConnected()) {
             //success
-            Toast.makeText(getContext(), "Conectado", Toast.LENGTH_SHORT).show();
+            if (getView()!= null)
+            Snackbar.make(getView(),"Conectado",1000).show();
         } else {
             //semConexão
-            Toast.makeText(getContext(), "Sem Conexão", Toast.LENGTH_SHORT).show();
+            if (getView()!=null)
+            Snackbar.make(getView(),"Sem Conexão",1000).show();
         }
     }
 
@@ -82,10 +84,10 @@ public class CotacaoListFragment extends BaseFragment<CoinsContract.CoinsListPre
         cotacaoAdapter = new ExchangeRateAdapter(getActivity(), coinsDomainList);
         rvListCotacao.setAdapter(cotacaoAdapter);
 
-        //LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        GridLayoutManager mGridLayoutManager = new GridLayoutManager(getContext(), 2);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+//        GridLayoutManager mGridLayoutManager = new GridLayoutManager(getContext(), 2);
         rvListCotacao.setHasFixedSize(true);
-        rvListCotacao.setLayoutManager(mGridLayoutManager);
+        rvListCotacao.setLayoutManager(layoutManager);
     }
 
     @Override

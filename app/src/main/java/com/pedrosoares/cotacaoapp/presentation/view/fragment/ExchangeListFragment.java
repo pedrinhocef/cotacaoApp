199e645f9825ed7dc22d1ac45c769d6816e76654
Carlos Henrique <chsl200293@gmail.com>
2019-03-20 21:05:19 -0300

    alterarndo icone do grid da toolbar

--- app/src/main/java/com/pedrosoares/cotacaoapp/presentation/view/fragment/ExchangeListFragment.java
+++ app/src/main/java/com/pedrosoares/cotacaoapp/presentation/view/fragment/ExchangeListFragment.java
@@ -1,8 +1,6 @@
 package com.pedrosoares.cotacaoapp.presentation.view.fragment;
 
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;

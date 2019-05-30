f4c9a9ea1248e28d63ce1f3cc24db87e74dd743d
Carlos Henrique <chsl200293@gmail.com>
2019-05-30 01:47:09 -0300

    add configuracoes para add pagamentos   - Branch: Add_Payments_Products_Managements

--- app/src/main/java/com/pedrosoares/cotacaoapp/MainActivity.java
+++ app/src/main/java/com/pedrosoares/cotacaoapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.pedrosoares.cotacaoapp;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 
+import com.android.vending.billing.IInAppBillingService;
 import com.google.android.gms.ads.MobileAds;
 import com.pedrosoares.cotacaoapp.core.base.BaseActivity;
 import com.pedrosoares.cotacaoapp.presentation.view.fragment.ExchangeListFragment;
@@ -9,12 +15,33 @@ import com.pedrosoares.cotacaoapp.presentation.view.fragment.ExchangeListFragmen
 import static com.pedrosoares.cotacaoapp.core.base.BaseFragment.setFragment;
 
 public class MainActivity extends BaseActivity {
+
+    ServiceConnection mServiceConn;
+    IInAppBillingService mService;
+    String Product_ID = "product_button_click";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         MobileAds.initialize(this, getString(R.string.id_app));
+
+        mServiceConn = new ServiceConnection() {
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                mService = null;
+            }
+            @Override
+            public void onServiceConnected(ComponentName name,
+                                           IBinder service) {
+                mService = IInAppBillingService.Stub.asInterface(service);
+            }
+        };
+
+        Intent serviceIntent = new Intent("com.android.vending.billing.InAppBillingService.BIND");
+        serviceIntent.setPackage("com.android.vending");
+        bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
 
 
         setFragment(R.id.frame_main_activity, new ExchangeListFragment(), this);
@@ -24,6 +51,14 @@ public class MainActivity extends BaseActivity {
     @Override
     public void onBackPressed(){
         moveTaskToBack(true);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mService != null) {
+            unbindService(mServiceConn);
+        }
     }
 
 }
